47c24126bcfae3a91bcc1162d21613d20149a603
benazirshaik11 <benazirbanushaik@gmail.com>
2026-01-17 16:19:15 +0000

    code coverage changes

--- src/test/java/com/banu/store/controller/HomeControllerTest.java
+++ src/test/java/com/banu/store/controller/HomeControllerTest.java
@@ -5,8 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
@@ -18,7 +17,7 @@ public class HomeControllerTest {
     void returnIndexPage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+                .andExpect(content().string("index"));
     }
 
 }
